c5aa5767be5eb4bdff5b09799ac00eaa638e672b
shfkd123 <shfkd123@gmail.com>
2023-07-06 10:08:29 +0900

    네이버 지도 api를 사용하여 지도 불러오기

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,15 +1,29 @@
-import React, { useEffect } from "react";
-import { Box } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { Box, Center } from "@chakra-ui/react";
+import Button from "../components/Button";
+import axios from "axios";
 
 const Map = () => {
+  const [sigungu, setSigungu] = useState([]);
+
   useEffect(() => {
     const mapDiv = document.getElementById("map");
-    new window.naver.maps.Map(mapDiv);
+    const mapOptions = {
+      center: new naver.maps.LatLng(36.3595704, 127.55399),
+      zoom: 8,
+    };
+    new window.naver.maps.Map(mapDiv, mapOptions);
+
+    const url = "./data/sigungu_test.json";
+    axios.get(url).then((response) => console.log(response));
   }, []);
 
   return (
     <Box>
-      <Box id="map" w="100%" h="900px" />
+      <Box id="map" w="100%" h="90vh" />
+      <Center>
+        <Button />
+      </Center>
     </Box>
   );
 };
